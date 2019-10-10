8128599ad3200d3bea85d638b3e08887e2863763
LeonardoCesca <leonardocescaf@gmail.com>
2019-10-10 11:04:09 -0300

    ADD arrow function into map

--- ES6/main.js
+++ ES6/main.js
@@ -1,25 +1,5 @@
-const arr = [1,2,3,4,5,8,9];
+const arr = [1,2,3,4,5,6];
 
-const newArr = arr.map(item => {
-    return item*2;
-})
+const newArr = arr.map(item => item * 2)
 
 console.log(newArr);
-
-const sum = arr.reduce((total, next) => {
-    return total + next;
-});
-
-console.log(sum);
-
-const filter = arr.filter(item => {
-    return item % 2 === 0;
-})
-
-console.log(filter);
-
-const find = arr.find(item => {
-    return item > 8;
-})
-
-console.log(find);
